afbf983f6313f4771a3b371a989d82db4456c5c3
Steven <dslesmes6@soy.sena.edu.co>
2024-10-11 11:28:34 -0500

    Se añadio handleTextChange

---
+++ comments2902093/src/componentes/ComentarioForm.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Card from './Card'
+import { useState } from 'react'
+import ComentarioCalificacion from './ComentarioCalificacion'
+
+
+const ComentarioForm = () => {
+
+    const[text, setText] = useState('')
+
+
+    const handleTextChange = (e) => {
+        setText(e.target.value)
+    }
+
+  return (
+    <Card>
+        <form>
+            <ComentarioCalificacion />
+        <div className='input-group'>
+        <input type="text"
+        value={text}
+        onChange={handleTextChange}
+        placeholder='Ingrese su comentario aqui' />
+        <button>
+            Guardar
+        </button>
+        </div>
+        </form>
+    </Card>
+  )
+}
+
+export default ComentarioForm
